f66ce40c29a566f46658f64c409b43fcd98c5279
mengweikang <mengweikang@yzw.cn>
2024-10-10 15:02:11 +0800

    first

---
+++ .prettierrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  singleQuote: true,
+  // 语句末尾分号
+  semi: true,
+  trailingComma: 'es5',
+  endOfLine:'auto',
+  // 对象中打印空格 默认true
+  // true: { foo: bar }
+  // false: {foo: bar}
+  bracketSpacing: true,
+  arrowParens: 'always',
+  // 使用tab缩进，默认false
+  // useTabs: true,
+  // tab缩进大小,默认为2
+  tabWidth: 2,
+  // 换行长度，默认80
+  printWidth: 120,
+  // 多行JSX标签闭合位置 单独一行
+  jsxBracketSameLine: false,
+};
