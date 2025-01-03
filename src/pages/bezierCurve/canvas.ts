8d635ae99ed3ef68acbd5e265d297bb6d8d03a28
mengweikang <mengweikang@yzw.cn>
2025-01-03 09:56:33 +0800

    贝塞尔曲线demo

--- src/pages/bezierCurve/canvas.ts
+++ src/pages/bezierCurve/canvas.ts
@@ -21,35 +21,26 @@ renderer.setPixelRatio(window.devicePixelRatio);
 document.body.appendChild(renderer.domElement);
 
 const scene = new THREE.Scene();
-scene.background = new THREE.Color(0xf3f3f3);
+scene.background = new THREE.Color(0xaaaaaa);
 
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 camera.position.z = 150;
 camera.position.y = 150;
 
-const groundGeometry = new THREE.PlaneGeometry(300, 300);
+const controls = new OrbitControls(camera, renderer.domElement);
+
+const axesHelper = new THREE.AxesHelper(120);
+scene.add(axesHelper);
+
+const groundGeometry = new THREE.PlaneGeometry(300, 100);
 groundGeometry.rotateX(0.5 * Math.PI);
-
-// 创建一个基础材质
-const material = new THREE.MeshBasicMaterial({ color: 0xcccccc, side: 2, transparent: true, opacity: 0.4 });
-// 创建网格对象（结合几何体和材质）
-const ground = new THREE.Mesh(groundGeometry, material);
-
+const groundMaterial = new THREE.MeshBasicMaterial({ color: 0xcccccc, side: 2, transparent: true, opacity: 0.8 });
+const ground = new THREE.Mesh(groundGeometry, groundMaterial);
 scene.add(ground);
 
 const vP1 = new THREE.Vector3(-100, 0, 0);
-
-const vP2 = new THREE.Vector3(0, 100, 0);
-
+const vP2 = new THREE.Vector3(-50, 100, 0);
 const vP3 = new THREE.Vector3(100, 0, 0);
-
-// const curve = new THREE.QuadraticBezierCurve3(vP1, vP2, vP3);
-
-// const lineMesh = new THREE.Mesh(
-//   new THREE.TubeGeometry(curve, 100, 2, 8, false),
-//   new THREE.MeshBasicMaterial({ color: 0x0001ff })
-// );
-// scene.add(lineMesh);
 
 const bezierCurve = new THREE.Line(
   new THREE.BufferGeometry(),
@@ -61,7 +52,6 @@ scene.add(bezierCurve);
 const P1 = createPoint({ position: vP1, color: 0xff0000 });
 const P2 = createPoint({ position: vP2, color: 0xff0000 });
 const P3 = createPoint({ position: vP3, color: 0xff0000 });
-
 scene.add(P1, P2, P3);
 
 const Q1 = createPoint({ position: new THREE.Vector3(0, 0, 0), color: 0x00ff00 });
@@ -69,27 +59,20 @@ const Q2 = createPoint({ position: new THREE.Vector3(0, 0, 0), color: 0x00ff00 }
 const Q3 = createPoint({ position: new THREE.Vector3(0, 0, 0), color: 0x00ff00 });
 scene.add(Q1, Q2, Q3);
 
-// const assistLine1 = new THREE.Line3(vP1, vP2);
-
 const assistLine1 = new THREE.Line(new THREE.BufferGeometry(), new THREE.LineBasicMaterial({ color: 0xff00ff }));
-scene.add(assistLine1);
-
 const assistLine2 = new THREE.Line(new THREE.BufferGeometry(), new THREE.LineBasicMaterial({ color: 0xff00ff }));
-scene.add(assistLine2);
-
 const assistLine3 = new THREE.Line(new THREE.BufferGeometry(), new THREE.LineBasicMaterial({ color: 0xff00ff }));
+scene.add(assistLine1);
+scene.add(assistLine2);
 scene.add(assistLine3);
-
-// 创建轨道控制器
-const controls = new OrbitControls(camera, renderer.domElement);
 
 const bezierCurveParams = { t: 0.5 };
 
 const gui = new GUI();
 gui.add(bezierCurveParams, 't', 0, 1, 0.01);
-gui.add(vP2, 'x', -100, 200, 1);
-gui.add(vP2, 'y', -100, 200, 1);
-gui.add(vP2, 'z', -100, 200, 1);
+gui.add(vP2, 'x', -200, 200, 1);
+gui.add(vP2, 'y', -200, 200, 1);
+gui.add(vP2, 'z', -200, 200, 1);
 
 function animate() {
   const vQ1 = new THREE.Vector3().copy(vP1).lerp(vP2, bezierCurveParams.t);
@@ -130,9 +113,10 @@ function animate() {
       3
     )
   );
-  requestIdleCallback(animate);
 
   controls.update();
   renderer.render(scene, camera);
+
+  requestIdleCallback(animate);
 }
 animate();
