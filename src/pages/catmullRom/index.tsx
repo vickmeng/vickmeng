5efebba731ae4c63e5d7f31f7582b4b8bb396c16
mengweikang <mengweikang@yzw.cn>
2025-01-07 09:30:33 +0800

    调整demo参数

--- src/pages/catmullRom/index.tsx
+++ src/pages/catmullRom/index.tsx
@@ -3,17 +3,7 @@ import './canvas';
 const CatmullRom = () => {
   return (
     <>
-      <h3 style={{ position: 'fixed' }}>
-        CatmullRom样条插值曲线
-        <br />
-        const PMinus2 = new THREE.Vector3(-3, 0, 0);
-        <br />
-        const PMinus1 = new THREE.Vector3(-1, 1, 0);
-        <br />
-        const P0 = new THREE.Vector3(1, 5, 0);
-        <br />
-        const P1 = new THREE.Vector3(3, 0, 0);
-      </h3>
+      <h3 style={{ position: 'fixed' }}>CatmullRom样条插值曲线</h3>
     </>
   );
 };
