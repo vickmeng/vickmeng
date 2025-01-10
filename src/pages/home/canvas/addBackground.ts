6a60deb8ea870d9300fef99fc0c6cf3cae8bb3d1
mengweikang <mengweikang@yzw.cn>
2025-01-10 17:10:22 +0800

    修改该ground

--- src/pages/home/canvas/addBackground.ts
+++ src/pages/home/canvas/addBackground.ts
@@ -1,17 +1,12 @@
-import { Mesh, MeshStandardMaterial, PlaneGeometry } from 'three';
+import { Mesh, MeshStandardMaterial, CircleGeometry } from 'three';
 import { scene } from '@/pages/home/canvas/core';
 
 export const addBackground = async () => {
-  const groundGeometry = new PlaneGeometry(80, 80);
+  const groundGeometry = new CircleGeometry(80, 32);
   groundGeometry.rotateX(-0.5 * Math.PI);
   const groundMaterial = new MeshStandardMaterial({ color: 0xcccccc });
   const ground = new Mesh(groundGeometry, groundMaterial);
   ground.receiveShadow = true;
   ground.castShadow = true;
   scene.add(ground);
-
-  // const ball = new Mesh(new SphereGeometry(10, 10), new MeshPhongMaterial({ color: 0xff0000 }));
-  // ball.receiveShadow = true;
-  // ball.castShadow = true;
-  // scene.add(ball);
 };
--- src/pages/home/canvas/addEvil.ts
+++ src/pages/home/canvas/addEvil.ts
@@ -20,25 +20,7 @@ export const addEvil = async () => {
     child.castShadow = true;
   });
 
-  // darkEvil.material = new MeshPhongMaterial({
-  //   color: 0xffffff,
-  //   specular: 0xffffff, // 高光颜色设为白色
-  //   shininess: 100, // 增加高光光泽度
-  // });
-  // darkEvil.receiveShadow = true;
-  // darkEvil.castShadow = true;
-  // (model.children[1] as Mesh).material = new MeshPhongMaterial({ color: 0xffffff });
-
   model.rotation.set(0, Math.PI / 2, 0);
   model.position.z = -25;
   scene.add(model);
-
-  // const gui = new GUI();
-  // gui.add(model.position, 'x', -100, 100, 1);
-  // gui.add(model.position, 'y', -100, 100, 1);
-  // gui.add(model.position, 'z', -100, 100, 1);
-  //
-  // gui.add(model.rotation, 'x', -2 * Math.PI, 2 * Math.PI, 0.1);
-  // gui.add(model.rotation, 'y', -2 * Math.PI, 2 * Math.PI, 0.1);
-  // gui.add(model.rotation, 'z', -2 * Math.PI, 2 * Math.PI, 0.1);
 };
